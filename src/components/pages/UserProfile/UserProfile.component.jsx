a1bcaa58530815de1834ee3516e4c925904cc652
liuqibasixseveneight <liuqiba.sixseveneight@gmail.com>
2021-09-30 18:17:00 +0100

    FEATURE: Initial UserProfile page

---
+++ src/components/pages/UserProfile/UserProfile.component.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { Wrapper } from "./UserProfile.styles";
+
+export default function UserProfile() {
+  return (
+    <>
+      <Wrapper>
+        <h1>User Profile</h1>
+      </Wrapper>
+    </>
+  );
+}
